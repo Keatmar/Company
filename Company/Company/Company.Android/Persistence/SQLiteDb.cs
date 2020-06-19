1c0ccd6ab01e9eb1db45c6872210536d0ae31c71
Marios Katsos <keatmar@gmail.com>
2020-06-19 17:18:55 +0300

    SQLite created

---
+++ Company/Company/Company.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using Company.Droid.Persistence;
+using Company.Persistance;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace Company.Droid.Persistence
+{
+	public class SQLiteDb : ISQLiteDb
+	{
+		public SQLiteAsyncConnection GetConnection()
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+			return new SQLiteAsyncConnection(path);
+		}
+	}
+}
---
+++ Company/Company/Company.iOS/Persistence/SQLiteDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using Company.iOS.Persistence;
+using Company.Persistance;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace Company.iOS.Persistence
+{
+	public class SQLiteDb : ISQLiteDb
+	{
+		public SQLiteAsyncConnection GetConnection()
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var path = Path.Combine(documentsPath, "Company.db3");
+
+			return new SQLiteAsyncConnection(path);
+		}
+	}
+}
